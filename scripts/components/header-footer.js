ab71ef0105d220be41f892ea92b1a22a8ab638ef
Nahuel Montero <nahue.montero94@gmail.com>
2022-10-18 12:58:26 -0300

    modifique el header

--- scripts/components/header-footer.js
+++ scripts/components/header-footer.js
@@ -1,6 +1,6 @@
 const labels = `
 <nav class="buttons">
-    <a id="home" href="/pages/home">Home</a>
+    <a id="home" href="../../index.html">Home</a>
     <a id="about" href="/pages/about">About us</a>
     <a id="events" href="/pages/events">Events</a>
     <a id="contact" href="/pages/form.html">Contact</a>
