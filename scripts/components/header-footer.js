8ad84fc29e46335f8e922355da1e96f696205bd0
Nahuel Montero <nahue.montero94@gmail.com>
2022-10-18 16:06:35 -0300

    agrego about us

--- scripts/components/header-footer.js
+++ scripts/components/header-footer.js
@@ -1,7 +1,7 @@
 const labels = `
 <nav class="buttons">
     <a id="home" href="../../index.html">Home</a>
-    <a id="about" href="/pages/about">About us</a>
+    <a id="about" href="/pages/about.html">About us</a>
     <a id="events" href="/pages/events">Events</a>
     <a id="contact" href="/pages/form.html">Contact</a>
 </nav>
