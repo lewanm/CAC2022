7881364515109fce887c46b7712a897832c09555
Nahuel Montero <nahue.montero94@gmail.com>
2022-10-23 12:33:41 -0300

    corrijo algunos valores de los css

--- scripts/pages/evento2.js
+++ scripts/pages/evento2.js
@@ -3,8 +3,14 @@ import map from '../components/events/mapCard.js'
 import weather from '../components/events/weatherCard.js'
 import image from '../components/events/imageCard.js'
 
-import {formateDate} from '../utils/date.js'
-import {obtainCoordinates} from '../utils/coordenates.js'
+import {formateDate} from '../utils/weather/date.js'
+import {obtainCoordinates} from '../utils/weather/coordenates.js'
+
+function getDate(days){
+    const tomorrow = new Date()
+    tomorrow.setDate(tomorrow.getDate() + days )
+    return tomorrow.toISOString()
+}
 
 const imgData = {
     'url' : "https://www.buenosaires.gob.ar/sites/gcaba/files/field/image/conociendo_ba_2_0.jpg",
@@ -14,7 +20,7 @@ const imgData = {
 const detailData = {
     'entrada' : true,
     'precio' : 450,
-    'fecha' : "2022-10-07T21:00:00.000Z",
+    'fecha' : getDate(3),
     'descripcion' : 'Desde la Fundación Cultural Argentino Japonesa que administra nuestro Jardín informamos que el sábado 5 de noviembre de 10 a 18:45 hs recibiremos donaciones y todo tipo de ayuda para enviar a Corrientes.'
 }
 
@@ -25,14 +31,26 @@ const mapData = {
     'localidad' : 'PALERMO',
 }
 
+const placeHolderMap = "https://www.google.com/maps/place/Nigeria/@9.0338725,8.677457,6z/data=!4m5!3m4!1s0x104e0baf7da48d0d:0x99a8fe4168c50bc8!8m2!3d9.081999!4d8.675277"
+
+const mapDataError = {
+    'url' : null,
+    'lugar' : 'Jardin Japones',
+    'direccion' : 'Av. Casares 3450',
+    'localidad' : 'PALERMO',
+}
+
+
 const weatherData = {
-    'coordinates' : mapData.url ? obtainCoordinates(mapData.url) : "Error",
+    'coordinates' : mapData.url ? obtainCoordinates(mapData.url) : obtainCoordinates(placeHolderMap),
     'date' : formateDate(detailData.fecha),
 
 }
 
+//const a = mapData.url ? mapData : mapDataError
+
 document.getElementById("image").innerHTML= image(imgData)
 document.getElementById("detail").innerHTML= detail(detailData)
-document.getElementById("weather").innerHTML = await weather(weatherData)
+//document.getElementById("weather").innerHTML = await weather(weatherData)
 document.getElementById("map").innerHTML = map(mapData)
 
